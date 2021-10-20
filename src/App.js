1ac96478cba6b355da80a55a8f05422d8ae019e4
Quyen Tran <qtrandev@gmail.com>
2021-10-19 22:01:41 -0400

    Update link target and fix console errors

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
       <header className="App-header">
         <h1> Web Links </h1>
         <p>
-          <a href="https://www.qtranfilms.com" target="_blank">qtranfilms.com</a> - Cinematographer<br/>
-          <a href="http://quyentran.com" target="_blank">quyentran.com</a> - Software developer<br/>
-          <a href="https://twitter.com/quyentran" target="_blank">twitter.com/quyentran</a> - Prolific tweeter<br/>
-          <a href="https://www.simplyeyes.com" target="_blank">simplyeyes.com</a> - Eye doctor<br/>
+          <a href="https://www.qtranfilms.com">qtranfilms.com</a> - Cinematographer<br/>
+          <a href="http://quyentran.com">quyentran.com</a> - Software developer<br/>
+          <a href="https://twitter.com/quyentran">twitter.com/quyentran</a> - Prolific tweeter<br/>
+          <a href="https://www.simplyeyes.com">simplyeyes.com</a> - Eye doctor<br/>
         </p>
         <Button variant="secondary" href="https://github.com/qtrandev/react-web-links">Source Code</Button>
       </header>
