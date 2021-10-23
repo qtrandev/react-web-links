7d8646d5d012ef6bb272715f1c905c86eeb20e09
Quyen Tran <qtrandev@gmail.com>
2021-10-22 23:17:25 -0400

    Move links list to static object

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import './App.css';
-import { Button } from 'react-bootstrap';
+import './App.css'
+import { Button } from 'react-bootstrap'
+
+const LINKS = [
+  { url: "https://www.qtranfilms.com", text: "qtranfilms.com", title: "Cinematographer" },
+  { url: "http://quyentran.com", text: "quyentran.com", title: "Software developer" },
+  { url: "https://twitter.com/quyentran", text: "twitter.com/quyentran", title: "Prolific tweeter" },
+  { url: "https://www.simplyeyes.com", text: "simplyeyes.com", title: "Eye doctor" },
+  { url: "https://quyentranacupuncture.co.uk", text: "quyentranacupuncture.co.uk", title: "Professional acupuncturist" },
+  { url: "https://www.quyen-tran.com", text: "quyen-tran.com", title: "Writer" }
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> Web Links </h1>
-        <p>
-          <a href="https://www.qtranfilms.com">qtranfilms.com</a> - Cinematographer<br/>
-          <a href="http://quyentran.com">quyentran.com</a> - Software developer<br/>
-          <a href="https://twitter.com/quyentran">twitter.com/quyentran</a> - Prolific tweeter<br/>
-          <a href="https://www.simplyeyes.com">simplyeyes.com</a> - Eye doctor<br/>
-          <a href="https://quyentranacupuncture.co.uk">quyentranacupuncture.co.uk</a> - Professional acupuncturist<br/>
-        </p>
+        <ul>
+          {
+            LINKS.map( link => {
+              return (
+              <li key={link.text}><a href={link.url}>{link.text}</a> - {link.title}<br/></li>
+              )
+            })
+          }
+        </ul>
         <Button variant="secondary" href="https://github.com/qtrandev/react-web-links">Source Code</Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
