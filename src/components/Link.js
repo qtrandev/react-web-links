38b50826f9ece3f383261a4b91e1a172d7be1955
Quyen Tran <qtrandev@gmail.com>
2021-10-28 10:13:12 -0400

    Simplify list item event to onClick

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Link({ link, onClick, onHover, onMouseOut }) {
   return (
-    <li onMouseOver={onHover} onMouseOut={onMouseOut}>
-      <a href={link.url}>{link.text}</a> - <span onClick={onClick}>{link.title}</span>
+    <li>
+      <a href={link.url}>{link.text}</a> - <span onClick={onClick} >{link.title}</span>
     </li>
   )
 }
