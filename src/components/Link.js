b6ca9c072be33c2312764aad0159b9d3891750d8
Quyen Tran <qtrandev@gmail.com>
2021-10-27 21:37:38 -0400

    Add additional onClick listener on list element

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Link({ link, onHover, onMouseOut }) {
   return (
-    <li onMouseOver={onHover} onMouseOut={onMouseOut}><a href={link.url}>{link.text}</a> - {link.title}<br/></li>
+    <li onClick={onHover} onMouseOver={onHover} onMouseOut={onMouseOut}>
+      <a href={link.url}>{link.text}</a> - {link.title}
+      <br />
+    </li>
   )
 }
