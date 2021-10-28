3cd5edb8bafb3c92e8561c061a2eb241e959c682
Quyen Tran <qtrandev@gmail.com>
2021-10-27 22:42:18 -0400

    Add scale-up animation

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,9 +2,8 @@ import React from 'react'
 
 export default function Link({ link, onClick, onHover, onMouseOut }) {
   return (
-    <li onMouseOver={onHover} onMouseOut={onMouseOut} onMou>
+    <li onMouseOver={onHover} onMouseOut={onMouseOut}>
       <a href={link.url}>{link.text}</a> - <span onClick={onClick}>{link.title}</span>
-      <br />
     </li>
   )
 }
